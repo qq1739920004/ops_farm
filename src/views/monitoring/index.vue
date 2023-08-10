82554dbf1d964257afb67f261c250dd5cecae3d3
Csheng <13814631823@163.com>
2023-08-10 19:33:24 +0800

    logo添加

--- src/views/monitoring/index.vue
+++ src/views/monitoring/index.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="app_container">
-    <el-button type="primary" icon="Plus">123</el-button>
-    <el-icon>
-      <Delete />
-    </el-icon>
-
-    <SvgIcon size="200" icon="vue"></SvgIcon>
-
-    <SvgIcon color="red" icon="language"></SvgIcon>
+    态势监控
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import SvgIcon from "@/components/SvgIcon/index.vue";
-let num = ref(0);
-console.log(num.value);
+
 </script>
 
 <style lang="scss" scoped></style>
