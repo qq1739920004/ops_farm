b43e9d6492ad04a276ad120d7ed653ccd715c70e
Csheng <13814631823@163.com>
2023-08-15 19:06:10 +0800

    layout修改

--- src/layout/components/Breadcrumb/index.vue
+++ src/layout/components/Breadcrumb/index.vue
@@ -1,19 +1,16 @@
 <template>
-  <el-breadcrumb class="h-[50px] flex items-center">
-    <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item, index) in breadcrumbs" :key="item.path">
-        <span
-          v-if="
-            item.redirect === 'noredirect' || index === breadcrumbs.length - 1
-          "
-          class="text-[var(--el-disabled-text-color)]"
-          >{{ item.meta.title}}</span
-        >
-        <a v-else @click.prevent="handleLink(item)">
-          {{ item.meta.title}}
-        </a>
-      </el-breadcrumb-item>
-    </transition-group>
+  <el-breadcrumb>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbs" :key="item.path">
+      <span
+        v-if="
+          item.redirect === 'noredirect' || index === breadcrumbs.length - 1
+        "
+        >{{ item.meta.title }}</span
+      >
+      <a v-else @click.prevent="handleLink(item)">
+        {{ item.meta.title }}
+      </a>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
@@ -23,14 +20,13 @@ import { useRoute, RouteLocationMatched } from "vue-router";
 import { compile } from "path-to-regexp";
 import router from "@/router";
 
-
 const currentRoute = useRoute();
 const pathCompile = (path: string) => {
   const { params } = currentRoute;
   const toPath = compile(path);
   return toPath(params);
 };
-
+console.log(currentRoute.matched,'---29')
 const breadcrumbs = ref([] as Array<RouteLocationMatched>);
 
 function getBreadcrumb() {
@@ -42,11 +38,11 @@ function getBreadcrumb() {
   });
 }
 
-
-
 function handleLink(item: any) {
   const { redirect, path } = item;
+
   if (redirect) {
+
     router.push(redirect).catch((err) => {
       console.warn(err);
     });
@@ -79,14 +75,22 @@ onBeforeMount(() => {
   font-size: 14px;
   line-height: 50px;
 }
+.el-breadcrumb__inner {
+  span {
+    // color: #c8c8c9;
+    color: #fff;
+  }
+}
 
 // 覆盖 element-plus 的样式
 .el-breadcrumb__inner,
 .el-breadcrumb__inner a {
   font-weight: 400 !important;
-  color:#fff;
+  // color: #fff;
+  color: #c8c8c9;
+
 }
-:deep(.el-breadcrumb__inner) {
-  color:#fff !important;
-}
+// :deep(.el-breadcrumb__inner) {
+//   color:#fff !important;
+// }
 </style>
