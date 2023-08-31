d06e82d82e8719419760f8a7f269380df2dcb519
caoxueling <331747511@qq.com>
2023-08-31 18:09:22 +0800

    指派

--- vite.config.ts
+++ vite.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
         // 测试服地址
         // target: "http://127.0.0.1:4523/m1/2885822-0-default",
         // Mock地址
-        target: 'http://140.207.166.210:9030/farm',
+        // target: 'http://140.207.166.210:9030/farm',
         // 位置点测试无网关
         // target:'http://140.207.166.210:9030/farmPoint',
         changeOrigin: true,
