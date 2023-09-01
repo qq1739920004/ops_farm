b2b765e6d4464fda7474b2387a20fbf8cf9382a2
Csheng <13814631823@163.com>
2023-09-01 13:48:21 +0800

    态势监控

--- src/views/monitoring/index.vue
+++ src/views/monitoring/index.vue
@@ -2,34 +2,47 @@
   <div class="map_conatiner">
     <div id="map"></div>
     <div class="map_utils">
-      <el-select v-model="value" @change="hangleSelectChange">
-        <el-option label="高德地图" :value="0" />
-        <el-option label="卫星地图" :value="1" />
-        <el-option label="google" :value="2" />
-        <el-option label="天地图" :value="3" />
+      <el-select v-model="mapTitleOptionsValue" @change="hangleSelectChange">
+        <el-option
+          v-for="item in mapTitleOptions"
+          :key="item.id"
+          :label="item.lable"
+          :value="item.id"
+        />
       </el-select>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { mapTitleLayers } from "./mapTitleLayers";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet.chinatmsproviders";
+import "leaflet.markercluster";
+import "leaflet.markercluster/dist/MarkerCluster.css"
+import "leaflet.markercluster/dist/MarkerCluster.Default.css"
+
+
 
 // import { monitorGet_API } from "@/api/monitoring";
 
 let map: any = null;
-
-let value = ref(1);
+// 地图瓦片图选项
+let mapTitleOptions = [
+  { id: 0, lable: "高德地图" },
+  { id: 1, lable: "卫星地图" },
+  { id: 2, lable: "google地图" },
+  { id: 3, lable: "天地图" },
+];
+let mapTitleOptionsValue = ref(mapTitleOptions[1].id);
 
 onMounted(() => {
   initMap();
 });
 
-// 初始化地图数据
+// 初始化加载地图
 function initMap() {
   map = L.map("map", {
     minZoom: 1, //最小缩放值
@@ -40,11 +53,62 @@ function initMap() {
     attributionControl: false, //是否启用地图属性控件
   });
   hangleSelectChange(); // 获取瓦片图
+
+  let data = [
+    { lng: 29.06097, lat: 111.93969 },
+    { lng: 29.06097, lat: 11.93969 },
+    { lng: 59.06097, lat: 111.93969 },
+  ];
+
+  for (var i = 0; i <= 100; i++) {
+    data.push({ lng: 59.06097, lat: 111.93969 });
+  }
+
+
+
+  let markerArr: any = [];
+  let markerGroup = L.markerClusterGroup();
+  data.forEach((item) => {
+    const marker = L.marker([item.lng, item.lat]);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    markerArr.push(marker);
+  });
+
+  
+
+
+  // console.log(markerGroup,'---80')
+  // let markerGroup = L.layerGroup(markerArr);
+  // markerGroup.addTo(map);
+  // markerGroup.clearLayers();
+
+  // ----------------------
+
+  // let markerGroup = L.layerGroup().addTo(map);
+  // data.forEach(item => {
+  //   let marker = L.marker([item.lng,item.lat]);
+  //   markerGroup.addLayer(marker);
+
+  // })
+
+  // let markerGroup = L.markerClusterGroup()
+  // data.forEach(item => {
+  //   let marker = L.marker([item.lng,item.lat]);
+  //   markerGroup.addLayer(marker)
+  // })
+
+  markerGroup.addLayers(markerArr)
+  markerGroup.addTo(map)
+ 
+  // markerGroup.removeLayer(markerArr[0])
+
+  // map.addLayer(markerGroup);
+
 }
 
 // 更改底地图
 const hangleSelectChange = () => {
-  handleMapChange(value.value);
+  handleMapChange(mapTitleOptionsValue.value);
 };
 function changeTileLayer(mapName = "GaoDe", mapType = "Satellite") {
   if (!map) {
@@ -52,8 +116,7 @@ function changeTileLayer(mapName = "GaoDe", mapType = "Satellite") {
     return;
   }
   let mapUrl = mapTitleLayers[mapName][mapType];
-
-  let options = reactive<any>({});
+  let options: any = {};
   options.subdomains = mapTitleLayers[mapName]["Subdomains"];
   if ("tms" in mapTitleLayers[mapName]) {
     options.tms = mapTitleLayers[mapName]["tms"];
