665900f89659543553c62966898c7c0e898dfa03
zhuangyixiao <1722465098@qq.com>
2023-09-06 11:41:30 +0800

    bushu

--- vite.config.ts
+++ vite.config.ts
@@ -48,9 +48,9 @@ export default defineConfig({
       '/dev-api': {
         // target: 'http://140.207.166.210:9030/gateway/farm',
         // target: "http://127.0.0.1:4523/m1/2885822-0-default",
-        // target: 'http://140.207.166.210:9030/farm',
+        target: 'http://140.207.166.210:9030/',
         // 测试服地址
-        target: "http://127.0.0.1:4523/m1/2885822-0-default",
+        // target: "http://127.0.0.1:4523/m1/2885822-0-default",
         // target: 'http://140.207.166.210:9030',
         // 位置点测试无网关
         // target:'http://140.207.166.210:9030/farmPoint',
