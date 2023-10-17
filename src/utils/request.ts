1f3583c623787f978b59e57ca3cff60a96b80728
Csheng <13814631823@163.com>
2023-10-17 14:39:21 +0800

    修改请求时间

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,7 +7,7 @@ let loadingInstance: any;
 // 创建 axios 实例
 const service = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
-  timeout: 50000,
+  timeout: 6000,
   headers: { 'Content-Type': 'application/json;charset=utf-8' }
 });
 
