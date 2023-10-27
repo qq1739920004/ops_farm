0f7b41c4b05c6b64341c3cd8ae2222232c8e4127
zhuangyixiao <1722465098@qq.com>
2023-10-27 10:20:45 +0800

    el-select数据懒加载

---
+++ src/utils/directives.js
@@ -0,0 +1,19 @@
+// import { createApp } from 'vue'
+// const app = createApp({})
+// app.directive('el-select-lazyloading', {
+//   bind(el, binding) {
+//     let SELECT_DOM = el.querySelector(
+//       ".el-select-dropdown .el-select-dropdown__wrap"
+//     );
+//     SELECT_DOM.addEventListener("scroll", function () {
+//       let condition = this.scrollHeight - this.scrollTop <= this.clientHeight;
+//       if (condition) {
+//         binding.value();
+//       }
+//     });
+//   }
+// })
+
+
+
+
