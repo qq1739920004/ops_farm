dc04b631ac8a923fa01ca82d4dca49b472a66b14
Csheng <13814631823@163.com>
2023-10-30 18:11:01 +0800

    1

--- src/api/machineryList/index.ts
+++ src/api/machineryList/index.ts
@@ -65,7 +65,7 @@ export function getSingleCarTrick_API(params: PageInfoObj) {
 // 文件存储
 export function getChildrenFileList_API(params: any) {
     return request<any, any>({
-        url: '/lu/log/filesForSn',
+        url: 'https://cloud.sinognss.com/lu/log/filesForSn',
         method: 'post',
         data: params,
         headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }
