ec3c2fcde0090c683123bf02b44a095b7451d2ef
zhuangyixiao <1722465098@qq.com>
2023-10-30 16:42:04 +0800

    录入经销商

--- vite.config.ts
+++ vite.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
     open: true, // 运行是否自动打开浏览器
     proxy: {
       '/dev-api/lu': {
-        target:'http://140.143.154.216:9051',
+        target: 'http://140.143.154.216:9051',
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(new RegExp("^/dev-api"), ""), // 替换 /dev-api 为 target 接口地址
