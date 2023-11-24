88d5f7f231b3a1a84d6b919073ce73de621b583e
zhuangyixiao <1722465098@qq.com>
2023-11-24 18:59:59 +0800

    跨域问题解决

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,16 +5,21 @@ declare module 'wkt' {
 
   // 定义Geometry类
   export class Geometry {
-      // 定义从WKT字符串解析的静态方法
-      static fromWKT(wkt: string): Geometry;
-      // 定义转换为WKT字符串的方法
-      toWKT(): string;
+    // 定义从WKT字符串解析的静态方法
+    static fromWKT(wkt: string): Geometry;
+    // 定义转换为WKT字符串的方法
+    toWKT(): string;
   }
-      //定义parse方法
+  //定义parse方法
   export function parse(wkt: string): Geometry;
   // 其他需要的类型声明可以在这里添加
 }
 declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
+declare module 'axios-jsonp' {
   const value: any;
   export default value;
 }
