c9385307248afdbe1c1b56424fc134830252f9f4
Csheng <13814631823@163.com>
2024-01-15 16:57:12 +0800

    socket处理

--- src/views/monitoring/index.vue
+++ src/views/monitoring/index.vue
@@ -189,7 +189,7 @@ import SinoMap from "@/components/SinoMap/index.vue";
 import SvgIcon from "@/components/SvgIcon/index.vue";
 import realTimeChart from "./components/realTimeChart.vue";
 import RemoteControl from "@/components/remoteAdjust/index.vue";
-import { reactive, ref, watch, onBeforeUnmount, computed} from "vue";
+import { reactive, ref, watch, onUnmounted, computed } from "vue";
 import useSocketStore from "@/store/socket";
 import { useRouter, useRoute } from "vue-router";
 import {
@@ -244,7 +244,7 @@ watch(
   },
   { deep: true }
 );
-onBeforeUnmount(() => {
+onUnmounted(() => {
   socketStore.close();
 });
 const labelWidth = computed(() => {
